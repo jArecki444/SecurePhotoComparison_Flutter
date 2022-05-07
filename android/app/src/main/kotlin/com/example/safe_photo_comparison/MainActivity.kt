bde55e5d76f83c724d918af23afb1dd7753986dd
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-05-07 20:11:05 +0200

    Platform permissions config of biometric feature

--- android/app/src/main/kotlin/com/example/safe_photo_comparison/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/safe_photo_comparison/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.safe_photo_comparison
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
-}
+  class MainActivity: FlutterFragmentActivity() {
+  }
